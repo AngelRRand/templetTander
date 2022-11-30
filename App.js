334da8f71e37ffb30ce972e6585ce9f31d99cccd
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-30 20:21:35 -0300

    test

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+
+      <Text>Holaaaa</Text>
+      
+    </View>
+  )
+}
+
+export default App
