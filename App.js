45e37442bcf92a1b18c68137c5016eeaec73af60
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-12-01 01:41:51 -0300

    sa

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/view/Home';
-
+zx
 
 
 const Stack = createNativeStackNavigator();
@@ -12,8 +12,10 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        
       </Stack.Navigator>
     </NavigationContainer>
+        
   );
 }
 
