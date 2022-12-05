43d44aeca21cf199038e538227f41b2fb5ae76a4
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-12-05 13:58:03 -0300

    add firestore

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { View, Text, Button, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import firestore from '@react-native-firebase/firestore'
 const Home = () => {
   const navigation = useNavigation()
+
+  const [data, setData] = useState()
+
+  async function loadData() {
+    try {
+      const users = await firestore().collection('Usuarios').get()
+      console.log(users.docs)
+      setData(users.docs)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  function renderItem({item}) {
+    return (
+      <View>
+        <Text>{item.data().nombre}</Text>
+
+      </View>
+    )
+  }
+
   return (
     <View>
-        <Text>HOME</Text>
-        <Button title='To go Chat' onPress={()=> navigation.navigate('Chat')}/>
+      <Text>HOME</Text>
+      <Button title='To go Chat' onPress={() => navigation.navigate('Chat')} />
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        />
+
     </View>
   )
 }
